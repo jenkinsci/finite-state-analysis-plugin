53bdaa8295cdd5170afa2577109a1be1a45b2358
cpfarherFinitestate <christian.pfarher@finitestate.io>
2025-08-05 12:11:39 -0300

    fixes

--- src/main/resources/io/jenkins/plugins/finitestateclt/FiniteStateSBOMImportRecorder/js/dynamic_events.js
+++ src/main/resources/io/jenkins/plugins/finitestateclt/FiniteStateSBOMImportRecorder/js/dynamic_events.js
@@ -1,29 +1,39 @@
 // Dynamic events for Finite State SBOM Import Recorder
-(function() {
-    'use strict';
+function toggleVersionField() {
+    var externalizableIdCheckbox = document.getElementById('sbomExternalizableId');
+    var versionDiv = document.getElementById('sbomVersion');
     
-    // Show/hide version field based on externalizableId checkbox
-    function toggleVersionField() {
-        var externalizableIdCheckbox = document.getElementById('externalizableId');
-        var versionDiv = document.getElementById('version');
-        
-        if (externalizableIdCheckbox && versionDiv) {
-            if (externalizableIdCheckbox.checked) {
-                versionDiv.style.display = 'none';
-            } else {
-                versionDiv.style.display = 'block';
-            }
+    if (externalizableIdCheckbox && versionDiv) {
+        if (externalizableIdCheckbox.checked) {
+            versionDiv.style.display = 'none';
+        } else {
+            versionDiv.style.display = 'block';
         }
     }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    toggleVersionField();
     
-    // Initialize on page load
-    document.addEventListener('DOMContentLoaded', function() {
-        toggleVersionField();
-        
-        // Add event listener to checkbox
-        var externalizableIdCheckbox = document.getElementById('externalizableId');
-        if (externalizableIdCheckbox) {
-            externalizableIdCheckbox.addEventListener('change', toggleVersionField);
-        }
-    });
+    // Add event listener to checkbox
+    var externalizableIdCheckbox = document.getElementById('sbomExternalizableId');
+    if (externalizableIdCheckbox) {
+        externalizableIdCheckbox.addEventListener('change', toggleVersionField);
+    }
+});
+
+(function() {
+    // your page initialization code here
+    // the DOM will be available here
+    
+    toggleVersionField();
 })();
+
+// Detect when your plugin is added as a post-build action
+Behaviour.specify(".finite-state-sbom-import", 'my-plugin', 100, function (element) {
+    // Trigger a custom event when your plugin's fields are loaded
+    var externalizableIdCheckbox = document.getElementById("sbomExternalizableId");
+    if (externalizableIdCheckbox) {
+        externalizableIdCheckbox.addEventListener("click", toggleVersionField);
+    }
+}); 
